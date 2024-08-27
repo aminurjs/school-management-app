6eabc10a17298fc75a4a2fc9b06c7946c89c1aa7
Aminur Rahman <dev.aminur@gmail.com>
2024-08-27 11:43:42 +0600

    navbar created

--- next-dashboard-ui/src/app/(dashboard)/layout.tsx
+++ next-dashboard-ui/src/app/(dashboard)/layout.tsx
@@ -1,10 +1,29 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-import "./globals.css";
 
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>{children}</div>;
+  return (
+    <div className="h-screen flex">
+      {/* LEFT */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block font-bold">School</span>
+        </Link>
+        <Menu />
+      </div>{" "}
+      {/* Right */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+        <Navbar />
+        {children}
+      </div>{" "}
+    </div>
+  );
 }
