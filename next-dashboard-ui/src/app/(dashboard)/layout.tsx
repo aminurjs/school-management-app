887c1d21fc37dfd3e2aeeb1ddcd62eadb2215bd1
Aminur Rahman <dev.aminur@gmail.com>
2024-09-02 22:36:10 +0600

    update design

--- next-dashboard-ui/src/app/(dashboard)/layout.tsx
+++ next-dashboard-ui/src/app/(dashboard)/layout.tsx
@@ -20,7 +20,7 @@ export default function DashboardLayout({
         <Menu />
       </div>{" "}
       {/* Right */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-auto flex flex-col">
         <Navbar />
         {children}
       </div>{" "}
