2a6cf98a5886fab63c5b26990e3ee659312e068a
Aminur Rahman <dev.aminur@gmail.com>
2024-09-03 11:15:20 +0600

    applied dark theme

--- next-dashboard-ui/src/app/(dashboard)/layout.tsx
+++ next-dashboard-ui/src/app/(dashboard)/layout.tsx
@@ -10,9 +10,9 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="min-h-screen flex">
+    <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 bg-white dark:bg-muted-accent dark:bg-[#09090B]">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 bg-white dark:bg-muted-accent overflow-y-auto hide-scrollbar">
         <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
           <Image src="/school-management.svg" alt="logo" width={32} height={32} />
           <span className="hidden lg:block font-bold">School</span>
@@ -21,7 +21,7 @@ export default function DashboardLayout({
       </div>{" "}
       {/* Right */}
       <div className="w-[86%]  md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] dark:bg-[#151518] flex flex-col">
-        <div className=" h-screen overflow-auto">
+        <div className="overflow-auto">
           <Navbar />
           {children}
         </div>
